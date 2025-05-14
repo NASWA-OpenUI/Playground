88f55395c57bd4b92a6fd004e165aa5c2826973b
grodnaswa <grod@naswa.org>
2025-05-14 15:24:07 -0600

    Update Program.cs

--- modules/benefits-admin/Program.cs
+++ modules/benefits-admin/Program.cs
@@ -1,7 +1,7 @@
-using BenefitsAdmin.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using BenefitsAdmin.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
