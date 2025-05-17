ddb9c6de7d83ef6cd61b92885f6ac4e85a9803d3
grodnaswa <grod@naswa.org>
2025-05-17 10:42:02 -0600

    Update setup-fix.sh

--- module-services/claimant-services/claimant/setup-fix.sh
+++ module-services/claimant-services/claimant/setup-fix.sh
@@ -1,7 +1,7 @@
 # Create public directory
-mkdir -p module-services/claimant-services/client/public
+mkdir -p module-services/claimant-services/claimant/public
 
 # Create an empty file as placeholder for favicon.ico
-touch module-services/claimant-services/client/public/favicon.ico
+touch module-services/claimant-services/client/claimant/favicon.ico
 
 echo "Claimant Services Client files created successfully."
