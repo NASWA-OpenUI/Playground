dda5c01f9f80de009f53006027b2da2828306eed
grodnaswa <grod@naswa.org>
2025-05-16 19:22:15 -0600

    dockerfile-updates

---
+++ modular-ui-system/claims-processing/src/services/claims-service.js
@@ -0,0 +1,9 @@
+// claims-processing/src/services/claims-service.js
+/**
+ * Claims Service for interacting with claims data
+ */
+const claimsService = {
+  // Service methods could be used in the future
+};
+
+module.exports = claimsService;
