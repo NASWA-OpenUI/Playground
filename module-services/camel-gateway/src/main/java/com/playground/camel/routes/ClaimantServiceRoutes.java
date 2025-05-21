0d96fad8a9891cb854ec97ec73e91754a25c8e9b
grodnaswa <grod@naswa.org>
2025-05-20 19:52:51 -0600

    Create ClaimantServiceRoutes.java

---
+++ module-services/camel-gateway/src/main/java/com/playground/camel/routes/ClaimantServiceRoutes.java
@@ -0,0 +1,33 @@
+package com.playground.camel.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClaimantServiceRoutes extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        // Set up a route to handle GraphQL queries to the claimant-services
+        from("direct:claimantServiceGraphQL")
+            .routeId("claimant-service-graphql")
+            .log("Routing GraphQL request to claimant-services: ${body}")
+            .removeHeaders("CamelHttp*") // Remove any existing HTTP headers
+            .setHeader("Content-Type", constant("application/json"))
+            .setHeader("Accept", constant("application/json"))
+            .to("http://claimant-services:3000/graphql")
+            .log("Received response from claimant-services");
+
+        // Create a REST endpoint that will front the GraphQL service
+        rest("/claimant")
+            .id("claimant-api")
+            .description("Claimant Services API")
+            .consumes("application/json")
+            .produces("application/json")
+            
+            // Forward all POST requests to the GraphQL endpoint
+            .post("/graphql")
+                .description("GraphQL endpoint for Claimant Services")
+                .to("direct:claimantServiceGraphQL");
+    }
+}
